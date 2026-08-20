80474687a00da7538adf6e27b90ed3c1e1e62809
Huy Nguyễn <huydocon1@gmail.com>
2026-08-20 16:01:56 +0700

    Generate Spring Boot Project (#8)

---
+++ src/main/java/com/sportily/SportilyApplication.java
@@ -0,0 +1,13 @@
+package com.sportily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportilyApplication.class, args);
+	}
+
+}
